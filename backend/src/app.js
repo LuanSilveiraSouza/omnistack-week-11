25b30f676a113c3bb17f104a174291822540db99
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-03-28 14:46:19 -0300

    Backend - data validation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const cors = require('cors'); 
+const {errors} = require('celebrate');
 const routes = require('./routes')
 
 const app = express();
@@ -9,6 +10,7 @@ const server = http.Server(app);
 app.use(cors())
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 server.listen(3333, () => {
     console.log("Server Listening at 3333");
